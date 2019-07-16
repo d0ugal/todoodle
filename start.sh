c735f588eeffb29e681c024ad84a59499b6ddb64
Dougal Matthews <dougal@redhat.com>
2019-07-16 21:11:42 +0100

    Re-add --detach when starting the container

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ set -o pipefail
 docker kill todoodle || true;
 docker rm todoodle || true;
 
-docker run \
+docker run -d \
   --name todoodle --network host --restart always \
   -e DB="todos:todos@tcp(192.168.1.200:3306)/todos?charset=utf8mb4&parseTime=True&loc=Local" \
   -e MQTT_BROKER="tcp://192.168.1.200:1883" \
